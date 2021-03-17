e4b1153384b92abdda73c80c6c4a26b5273e32bd
Zing <zingimmick@outlook.com>
2021-03-17 17:20:13 +0800

    Refactor ServiceProvider (#6)

--- src/VoteServiceProvider.php
+++ src/VoteServiceProvider.php
@@ -4,48 +4,10 @@ declare(strict_types=1);
 
 namespace LaravelInteraction\Vote;
 
-use Illuminate\Support\ServiceProvider;
+use LaravelInteraction\Support\InteractionList;
+use LaravelInteraction\Support\InteractionServiceProvider;
 
-class VoteServiceProvider extends ServiceProvider
+class VoteServiceProvider extends InteractionServiceProvider
 {
-    public function boot(): void
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes(
-                [
-                    $this->getConfigPath() => config_path('vote.php'),
-                ],
-                'vote-config'
-            );
-            $this->publishes(
-                [
-                    $this->getMigrationsPath() => database_path('migrations'),
-                ],
-                'vote-migrations'
-            );
-            if ($this->shouldLoadMigrations()) {
-                $this->loadMigrationsFrom($this->getMigrationsPath());
-            }
-        }
-    }
-
-    public function register(): void
-    {
-        $this->mergeConfigFrom($this->getConfigPath(), 'vote');
-    }
-
-    protected function getConfigPath(): string
-    {
-        return __DIR__ . '/../config/vote.php';
-    }
-
-    protected function getMigrationsPath(): string
-    {
-        return __DIR__ . '/../migrations';
-    }
-
-    private function shouldLoadMigrations(): bool
-    {
-        return (bool) config('vote.load_migrations');
-    }
+    protected $interaction = InteractionList::VOTE;
 }
